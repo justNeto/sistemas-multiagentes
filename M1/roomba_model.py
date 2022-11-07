da89b3cb9d18d179ea9b5be33ee2cd8b9ef872c1
justNeto <continental.lelgg.2b@gmail.com>
2022-11-07 13:45:34 -0600

    Added the roomba model. Still to edit

--- M1/roomba_model.py
+++ M1/roomba_model.py
@@ -1,13 +1,5 @@
 import mesa
 
-def compute_gini(model):
-    agent_wealths = [agent.wealth for agent in model.schedule.agents]
-    x = sorted(agent_wealths)
-    N = model.num_agents
-    B = sum(xi * (N - i) for i, xi in enumerate(x)) / (N * sum(x))
-    return 1 + (1 / N) - 2 * B
-
-
 class MoneyAgent(mesa.Agent):
     """An agent with fixed initial wealth."""
 
