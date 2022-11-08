30c72187ff0a11bfc0e12af328e088648cee30f4
justNeto <continental.lelgg.2b@gmail.com>
2022-11-07 18:06:07 -0600

    Commited roomba model

--- M1/roomba_model.py
+++ M1/roomba_model.py
@@ -1,54 +1,82 @@
 import mesa
 
-class MoneyAgent(mesa.Agent):
-    """An agent with fixed initial wealth."""
+class DirtAgent(mesa.Agent):
+    """An agent that sims dirt"""
 
+    def __init__(self, unique_id, dirtyness, model):
+        super().__init__(unique_id, dirtyness, model)
+
+class RoombaAgent(mesa.Agent):
+    """An agent that sims a roomba"""
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.wealth = 1
 
     def move(self):
         possible_steps = self.model.grid.get_neighborhood(
             self.pos, moore=True, include_center=False
         )
+
+        print(possible_steps)
+        break
+
         new_position = self.random.choice(possible_steps)
         self.model.grid.move_agent(self, new_position)
 
-    def give_money(self):
+    # def clean(self):
+
+    def clean(self):
         cellmates = self.model.grid.get_cell_list_contents([self.pos])
+
         if len(cellmates) > 1:
-            other = self.random.choice(cellmates)
-            other.wealth += 1
-            self.wealth -= 1
+            print(cellmates)
+            # other = self.random.choice(cellmates)
+            # other.wealth += 1
+            # self.wealth -= 1
 
     def step(self):
-        self.move()
-        if self.wealth > 0:
-            self.give_money()
+        will_move = self.move() # check if agent can move
+        if (will_move):
+            self.clean()
+
+class CleaningModel(mesa.Model):
+    """A model that creates the space and spawns the required agents"""
+
+    def __init__(self, num_of_roombas, dirt_percentage, room_width, room_height, max_num_batches):
+
+        # Before doing anything check data type of dirt_percentage
+        if ((!isinstance(dirt_percentage, int) and (0 <= dirt_percentage) ):
+            print("Error: not a percentage betw
+            break
+
+        # Declare initial data of the model
+        self.roombas = num_of_roombas
+        self.dirt = dirt_percentage
+        self.batch = max_num_batches
+
+        self.grid = mesa.space.MultiGrid(room_width, room_height, True) # create the space of a width and height room_width, room_height
+
+        self.schedule = mesa.time.RandomActivation(self) # scheduler for steps
+        self.running = True # running
+
+        # Populate the grid with dirt agents
+
+        for i in range():
 
 
-class MoneyModel(mesa.Model):
-    """A model with some number of agents."""
+        # Create all the roomba agents
+        for i in range(self.roombas):
+            agent = RoombaAgent(i, self) # constructor RoombaAgent(id)
+            self.schedule.add(agent) # adds agent to scheduler
 
-    def __init__(self, N, width, height):
-        self.num_agents = N
-        self.grid = mesa.space.MultiGrid(width, height, True)
-        self.schedule = mesa.time.RandomActivation(self)
-        self.running = True
-
-        # Create agents
-        for i in range(self.num_agents):
-            a = MoneyAgent(i, self)
-            self.schedule.add(a)
             # Add the agent to a random grid cell
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
-            self.grid.place_agent(a, (x, y))
+            self.grid.place_agent(agent, (x, y))
 
-        self.datacollector = mesa.DataCollector(
-            model_reporters={"Gini": compute_gini}, agent_reporters={"Wealth": "wealth"}
-        )
+        # self.datacollector = mesa.DataCollector(
+        #     model_reporters={"Gini": compute_gini}, agent_reporters={"Wealth": "wealth"}
+        # )
 
     def step(self):
-        self.datacollector.collect(self)
+        #self.datacollector.collect(self)
         self.schedule.step()
