537f63b1694b008fcadfefeaca4fedc630697546
justNeto <continental.lelgg.2b@gmail.com>
2022-11-20 13:45:00 -0600

    Added grid and coloring. Next TODO: spawning areas

--- M3/inter_model.py
+++ M3/inter_model.py
@@ -6,15 +6,26 @@ import random
 class Sidewalk(mesa.Agent):
     """An agent that sims the sidewalk of the street"""
 
-    def __init__(self, model):
-        super().__init__(model)
-
-
-class Sidewalk(mesa.Agent):
-    """An agent that sims the sidewalk of the street"""
-
     def __init__(self, unique_id, model):
-        super().__init__(model)
+        super().__init__(unique_id, model)
+
+class Ambulance(mesa.Agent):
+    """An ambulance agent"""
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.status = 0
+
+class Sensor(mesa.Agent):
+    """An agent that actually senses a car and does stuff with it """
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.status = 0
+
+class TrafficLight(mesa.Agent):
+    """An agent that sims the light colors but is only for painting of the street"""
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.status = 0
 
 class Car(mesa.Agent):
     """An agent that sims a roomba"""
@@ -76,21 +87,152 @@ class IntersectionModel(mesa.Model):
         self.max_cars = max_cars_num
         self.curr_cars = 0
 
-        # Paint the grid
+        self.kill_agents = [] # agents to kill after each step
+        self.grid = mesa.space.MultiGrid(50, 50, False) # create the space of a width and height room_width, room_height and no torodoidal
+        self.schedule = mesa.time.RandomActivation(self) # scheduler for steps
+        self.running = True # running while this is true
+
+        self.s_one = []
+        self.s_two = []
+        self.s_three = []
+        self.s_four = []
+
+        # Intersection via y:
+        x_val = np.union1d(np.array([i for i in range(19)]), np.array([i for i in range(24, 50)]))
+        y_val = np.union1d(np.array([i for i in range(22)]), np.array([i for i in range(29, 50)]))
+
+        i = 0
+        for x in x_val:
+            for y in y_val:
+                #print(f"x: {x} ; y = {y}")
+                print(f"Unique sidewalk id: {i}")
+                agent = Sidewalk(i, self) # constructor for Sidewalk
+                self.schedule.add(agent) # adds agent to scheduler
+                self.grid.place_agent(agent, (x, y))
+                i += 1
+
+        # Paint the traffic light
+        tl_one_x = [18]
+        tl_one_y = [22,23,24]
+
+        for x in tl_one_x:
+            for y in tl_one_y:
+                print(f"Unique traffic id: {i}")
+                agent = TrafficLight(i, self) # constructor for Sidewalk
+                self.schedule.add(agent) # adds agent to scheduler
+                self.grid.place_agent(agent, (x, y))
+                i += 1
+
+        sensor_one_x = [18]
+        sensor_one_y = [i for i in range(22,29) ]
+
+        for x in sensor_one_x:
+            for y in sensor_one_y:
+                print(f"Unique sensor id: {i}")
+                agent = Sensor(i, self) # constructor for Sidewalk
+                self.s_one.append(agent)
+                self.schedule.add(agent) # adds agent to scheduler
+                self.grid.place_agent(agent, (x, y))
+                i += 1
+
+        tl_two_x = [24]
+        tl_two_y = [26,27,28]
+
+        for x in tl_two_x:
+            for y in tl_two_y:
+                print(f"Unique traffic id: {i}")
+                agent = TrafficLight(i, self) # constructor for Sidewalk
+                self.schedule.add(agent) # adds agent to scheduler
+                self.grid.place_agent(agent, (x, y))
+                i += 1
+
+        sensor_two_x = [24]
+        sensor_two_y = [i for i in range(22,29) ]
+
+        for x in sensor_two_x:
+            for y in sensor_two_y:
+                print(f"Unique sensor id: {i}")
+                agent = Sensor(i, self) # constructor for Sidewalk
+                self.s_two.append(agent)
+                self.schedule.add(agent) # adds agent to scheduler
+                self.grid.place_agent(agent, (x, y))
+                i += 1
+
+        tl_three_x = [21, 22, 23]
+        tl_three_y = [21]
+
+        for x in tl_three_x:
+            for y in tl_three_y:
+                print(f"Unique traffic id: {i}")
+                agent = TrafficLight(i, self) # constructor for Sidewalk
+                self.schedule.add(agent) # adds agent to scheduler
+                self.grid.place_agent(agent, (x, y))
+                i += 1
+
+        sensor_three_x = [i for i in range(19, 24)]
+        sensor_three_y = [21]
+
+        for x in sensor_three_x:
+            for y in sensor_three_y:
+                print(f"Unique sensor id: {i}")
+                agent = Sensor(i, self) # constructor for Sidewalk
+                self.s_three.append(agent)
+                self.schedule.add(agent) # adds agent to scheduler
+                self.grid.place_agent(agent, (x, y))
+                i += 1
+
+        tl_four_x = [19, 20, 21]
+        tl_four_y = [29]
+
+        for x in tl_four_x:
+            for y in tl_four_y:
+                print(f"Unique traffic id: {i}")
+                agent = TrafficLight(i, self) # constructor for Sidewalk
+                self.schedule.add(agent) # adds agent to scheduler
+                self.grid.place_agent(agent, (x, y))
+                i += 1
+
+        sensor_four_x = [i for i in range(19, 24)]
+        sensor_four_y = [29]
+
+        for x in sensor_four_x:
+            for y in sensor_four_y:
+                print(f"Unique sensor id: {i}")
+                agent = Sensor(i, self) # constructor for Sidewalk
+                print(agent)
+                print(agent.unique_id)
+                self.s_four.append(agent)
+                self.schedule.add(agent) # adds agent to scheduler
+                self.grid.place_agent(agent, (x, y))
+                i += 1
+
+        # Print all sensor arrays
+        for val in self.s_one:
+            print(val.unique_id)
+        for val in self.s_two:
+            print(val.unique_id)
+        for val in self.s_three:
+            print(val.unique_id)
+        for val in self.s_four:
+            print(val.unique_id)
+
+        # Spawning areas
 
         # self.datacollector = mesa.DataCollector(
         #         model_reporters={"Current_steps": get_current_model_steps},
         #         agent_reporters={}
         # )
 
-
     def step(self):
         # self.datacollector.collect(self)
+
         if self.curr_cars == self.max_cars:
             print("Do not spawn more cars and step")
             self.schedule.step() # continue the simulation
         else:
             # Spawn cars if cars can be spawned
+            # Check spawning positions in grid if empty
+            # Spawn cars
             self.schedule.step()
 
             while self.kill_agents != []:
--- M3/vis.py
+++ M3/vis.py
@@ -1,4 +1,4 @@
-from roomba_model import *
+from inter_model import *
 import mesa
 
 def agent_portrayal(agent):
@@ -11,32 +11,41 @@ def agent_portrayal(agent):
     if isinstance(agent, Sidewalk):
         portrayal["Color"] = "brown"
         portrayal["Layer"] = 0
-        portrayal["r"] = 0.5
+        portrayal["r"] = 0.3
     elif isinstance(agent, Car):
         portrayal["Color"] = "blue"
         portrayal["Layer"] = 1
         portrayal["r"] = 0.3
+    elif isinstance(agent, Ambulance):
+            portrayal["Color"] = "blue"
+            portrayal["Layer"] = 1
+            portrayal["r"] = 0.5
     elif isinstance(agent, TrafficLight):
-        if (agent.status == 0): # if stop in step
+        if (agent.status == 1): # if stop in step
             portrayal["Color"] = "red"
             portrayal["Layer"] = 1
-            portrayal["r"] = 0.3
-        elif (agent.status == 1): #
+            portrayal["r"] = 0.5
+        elif (agent.status == 2): #
             portrayal["Color"] = "yellow"
             portrayal["Layer"] = 1
-            portrayal["r"] = 0.3
-        elif (agent.status == 2): #
+            portrayal["r"] = 0.5
+        elif (agent.status == 3): #
             portrayal["Color"] = "green"
             portrayal["Layer"] = 1
-            portrayal["r"] = 0.3
+            portrayal["r"] = 0.5
         else:
             portrayal["Color"] = "black"
             portrayal["Layer"] = 1
-            portrayal["r"] = 0.3
+            portrayal["r"] = 0.5
+    elif isinstance(agent, Sensor):
+            portrayal["Color"] = "blue"
+            portrayal["Layer"] = 1
+            portrayal["r"] = 0
+
 
     return portrayal
 
-grid = mesa.visualization.CanvasGrid(agent_portrayal, 25, 20, 500, 500)
+grid = mesa.visualization.CanvasGrid(agent_portrayal, 50, 50, 500, 500)
 
 server = mesa.visualization.ModularServer(
         IntersectionModel, [grid], "Intersection model", {"max_cars_num" : 20}
